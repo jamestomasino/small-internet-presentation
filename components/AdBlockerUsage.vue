48216e66564ba626fb62236b999c08ec96aa9faf
James Tomasino <james@tomasino.org>
2022-07-21 18:09:12 +0000

    adds survey sections

---
+++ components/AdBlockerUsage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img class="full" src="~assets/images/ad-blocker-usage.png">
+  </div>
+</template>
---
+++ components/SurveyParticipation.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img class="full" src="~assets/images/survey-participation.png">
+  </div>
+</template>
