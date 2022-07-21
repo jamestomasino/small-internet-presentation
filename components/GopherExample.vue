094042d621dce01a532b8d81adbf05594271c143
James Tomasino <james@tomasino.org>
2022-07-21 18:15:22 +0000

    adds gopher example video

---
+++ components/GopherExample.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="center">
+    <client-only>
+      <vimeo-player
+        :options="{ loop: true }"
+        video-url="732203097"
+        muted="true"
+        player-width="1920"
+        player-height="1050"
+        autoplay
+      />
+    </client-only>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -42,6 +42,7 @@ export default {
         'HowDoesItWork',
         'TheProtocols',
         'Gopher',
+        'GopherExample',
         'Gemini',
         'GeminiExample',
         'TheCommunities'
