21dbadf1a4fe3fe90257ccd3ffacb130c9b08522
James Tomasino <james@tomasino.org>
2022-07-23 10:28:44 +0000

    use gemini local video in local testing

--- components/GeminiExample.vue
+++ components/GeminiExample.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="center">
     <client-only>
+      <VideoPlayer
+        v-if="$hostname === 'localhost'"
+        src="/video/gemini-explore.mp4"
+        autoplay="true"
+        loop="true"
+        muted="true"
+      />
       <vimeo-player
+        v-if="$hostname !== 'localhost'"
         :options="{ loop: true }"
         video-url="732171575"
         muted="true"
-        player-width="1280"
-        player-height="970"
+        player-width="1920"
+        player-height="1050"
         autoplay
       />
     </client-only>
   </div>
 </template>
+
+<script>
+import VideoPlayer from 'nuxt-video-player'
+require('nuxt-video-player/src/assets/css/main.css')
+export default {
+  components: {
+    VideoPlayer
+  }
+}
+</script>
