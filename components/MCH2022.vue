29fffb657a990170c82cf2dcb8c40018023ec5af
James Tomasino <james@tomasino.org>
2022-07-23 10:50:43 +0000

    survey images fixed

--- components/MCH2022.vue
+++ components/MCH2022.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="center padding">
+  <div class="mch2022 center padding">
     <p>
       The <span class="yellow">web is a mess</span>, bloated with
       data-gathering trackers, predatory UX, massive resource loads, and it
-      is absorbing everything it touches. The
-      <span class="pink">Small Internet</span>
+      is absorbing everything it touches.
+    </p>
+    <p>
+      The <span class="pink">Small Internet</span>
       is a counter-cultural movement to wrangle things back
       under control via minimalism, hands-on participation, and good old
-      fashioned conversation. At its heart are technologies like the venerable
+      fashioned conversation.
+    </p>
+    <p>
+      At its heart are technologies like the venerable
       <span class="green">Gopher protocol</span> or the new <span class="green">Gemini protocol</span>
       offering a refuge and a place to dream of a better future.
     </p>
   </div>
 </template>
+
+<style lang="scss">
+.mch2022 {
+  p {
+    font-size: 60px;
+    line-height: 70px;
+
+    &:not(:last-of-type) {
+      padding-bottom: 1em;
+    }
+  }
+}
+</style>
